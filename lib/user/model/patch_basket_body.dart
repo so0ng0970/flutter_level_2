51cec5c2f297d022a7d812ea63739f341dda429f
soonger3306 <soonger3306@daum.net>
2023-06-20 23:42:59 +0900

    Optimistic Response

--- lib/user/model/patch_basket_body.dart
+++ lib/user/model/patch_basket_body.dart
@@ -3,13 +3,26 @@ part 'patch_basket_body.g.dart';
 
 @JsonSerializable()
 class PatchBasketBody {
-  final String productId;
-  final int count;
+  final List<PatchBasketBodyBasket> basket;
 
   PatchBasketBody({
+    required this.basket,
+  });
+
+  Map<String, dynamic> toJson() => _$PatchBasketBodyToJson(this);
+}
+
+@JsonSerializable()
+class PatchBasketBodyBasket {
+  final String productId;
+  final int count;
+  PatchBasketBodyBasket({
     required this.productId,
     required this.count,
   });
 
-  Map<String, dynamic> toJson() => _$PatchBasketBodyToJson(this);
+  factory PatchBasketBodyBasket.fromJson(Map<String, dynamic> json) =>
+      _$PatchBasketBodyBasketFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PatchBasketBodyBasketToJson(this);
 }
