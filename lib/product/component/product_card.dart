d677519a2802a54f766b7b79f08e5264b583d8bf
soonger3306 <soonger3306@daum.net>
2023-06-22 19:35:39 +0900

    feat: footer

--- lib/product/component/product_card.dart
+++ lib/product/component/product_card.dart
@@ -10,9 +10,13 @@ class ProductCard extends StatelessWidget {
   final String name;
   final String detail;
   final int price;
+  final VoidCallback? onSubtract;
+  final VoidCallback? onAdd;
 
   const ProductCard({
     Key? key,
+    this.onSubtract,
+    this.onAdd,
     required this.image,
     required this.name,
     required this.detail,
@@ -21,6 +25,8 @@ class ProductCard extends StatelessWidget {
 
   factory ProductCard.fromProductModel({
     required ProductModel model,
+    VoidCallback? onSubtract,
+    VoidCallback? onAdd,
   }) {
     return ProductCard(
       image: Image.network(
@@ -32,6 +38,8 @@ class ProductCard extends StatelessWidget {
       name: model.name,
       detail: model.detail,
       price: model.price,
+      onSubtract: onSubtract,
+      onAdd: onAdd,
     );
   }
 
@@ -54,47 +62,133 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // IntrinsicHeight - 내부에 있는 위젯들이 최대크기인 위젯만큼 차지하게 해준다
-    return IntrinsicHeight(
-      child: Row(
-        children: [
-          ClipRRect(borderRadius: BorderRadius.circular(8.0), child: image),
-          const SizedBox(
-            width: 16.0,
+    return Column(
+      children: [
+        IntrinsicHeight(
+          child: Row(
+            children: [
+              ClipRRect(borderRadius: BorderRadius.circular(8.0), child: image),
+              const SizedBox(
+                width: 16.0,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      detail,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        color: BODY_TEXT_COLOR,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    Text(
+                      ' $price',
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(
+                        color: PRIMARY_COLOR,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Text(
-                  detail,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: const TextStyle(
-                    color: BODY_TEXT_COLOR,
-                    fontSize: 14.0,
-                  ),
-                ),
-                Text(
-                  ' $price',
-                  textAlign: TextAlign.right,
-                  style: const TextStyle(
-                    color: PRIMARY_COLOR,
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+        ),
+        if (onSubtract != null && onAdd != null)
+          _Footer(
+            onSubtract: onSubtract!,
+            onAdd: onAdd!,
+            count: count,
+            total: total,
+          ),
+      ],
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final String total;
+  final int count;
+  final VoidCallback onSubtract;
+  final VoidCallback onAdd;
+
+  const _Footer({
+    required this.onSubtract,
+    required this.onAdd,
+    required this.count,
+    required this.total,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            '총액 $total',
+            style: const TextStyle(
+              color: PRIMARY_COLOR,
+              fontWeight: FontWeight.w500,
             ),
           ),
-        ],
+        ),
+        Row(
+          children: [
+            //
+            renderButton(
+              icon: Icons.remove,
+              onTap: onSubtract,
+            ),
+            Text(
+              count.toString(),
+              style: const TextStyle(
+                color: PRIMARY_COLOR,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            renderButton(
+              icon: Icons.add,
+              onTap: onAdd,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget renderButton({
+    required IconData icon,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          8.0,
+        ),
+        border: Border.all(
+          color: PRIMARY_COLOR,
+          width: 1.0,
+        ),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Icon(
+          icon,
+          color: PRIMARY_COLOR,
+        ),
       ),
     );
   }
